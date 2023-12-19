ec29986c32379aab87320bb9d514d5fc264556d0
Gomez-Oscar <alexgomez212121@gmail.com>
2023-12-19 14:20:39 -0500

    agregando titulo e icono a index.html

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from '../pages/register/Register';
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
