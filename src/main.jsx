a68c7c4f1e8ef97cd9c41010124be72697a36c4f
Gomez-Oscar <alexgomez212121@gmail.com>
2023-12-20 00:27:50 -0500

    implementando interfaz inicial de register

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './router/AppRouter';
+import './main.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<AppRouter />);
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from '../pages/register/Register';
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-          <Route path='register' element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route path='register' element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 };
 
