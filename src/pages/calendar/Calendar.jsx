18132588a86d5def9229db92d09191e6113d319b
Gomez-Oscar <alexgomez212121@gmail.com>
2023-12-25 11:58:42 -0500

    just making some changes to the margin

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
-import { Box, Grid, Button, Text, ButtonGroup } from "@chakra-ui/react";
-import { format, addDays } from "date-fns";
-import { Heading } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Grid, Button, Text, ButtonGroup } from '@chakra-ui/react';
+import { format, addDays } from 'date-fns';
+import { Heading } from '@chakra-ui/react';
+import './calendar.scss';
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedHour, setSelectedHour] = useState(null);
 
-  const handleDateChange = (newDate) => {
+  const handleDateChange = newDate => {
     setSelectedDate(newDate);
+    console.log(newDate);
     setSelectedHour(null); // Reiniciar la hora al cambiar la fecha
   };
 
-  const handleHourSelection = (hour) => {
+  const handleHourSelection = hour => {
     setSelectedHour(hour);
+    console.log(hour);
   };
 
   const renderCalendar = () => {
@@ -30,17 +33,17 @@ const Calendar = () => {
     }
 
     return (
-      <Grid templateColumns="repeat(7, 1fr)" gap={2}>
-        {daysInMonth.map((day) => (
+      <Grid templateColumns='repeat(7, 1fr)' gap={2}>
+        {daysInMonth.map(day => (
           <Button
             key={day}
             variant={
-              day.getDate() === selectedDate.getDate() ? "solid" : "outline"
+              day.getDate() === selectedDate.getDate() ? 'solid' : 'outline'
             }
-            colorScheme="teal"
+            colorScheme='teal'
             onClick={() => handleDateChange(day)}
           >
-            {format(day, "dd")}
+            {format(day, 'dd')}
           </Button>
         ))}
       </Grid>
@@ -49,23 +52,24 @@ const Calendar = () => {
 
   const renderHours = () => {
     const hoursOfDay = [
-      "09:00",
-      "10:00",
-      "11:00",
-      "12:00",
-      "14:00",
-      "15:00",
-      "16:00",
-      "17:00",
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
     ];
 
     return (
-      <Grid templateColumns="repeat(4, 1fr)" gap={2} mt={4}>
-        {hoursOfDay.map((hour) => (
+      <Grid templateColumns='repeat(4, 1fr)' gap={2} mt={4}>
+        {hoursOfDay.map(hour => (
           <Button
             key={hour}
-            variant={hour === selectedHour ? "solid" : "outline"}
-            colorScheme="teal"
+            variant={hour === selectedHour ? 'solid' : 'outline'}
+            colorScheme='teal'
             onClick={() => handleHourSelection(hour)}
           >
             {hour}
@@ -76,24 +80,44 @@ const Calendar = () => {
   };
 
   return (
-    <Box p={4} className="calendar">
+    <Box p={4} className='calendar'>
       <Heading
-        as="h2"
+        as='h2'
         mb={4}
-        size="lg"
-        display={"flex"}
-        justifyContent={"center"}
+        size='lg'
+        display={'flex'}
+        justifyContent={'center'}
       >
         Reserva tu cita
       </Heading>
-      <Text fontSize="xl" mb={4}>
-        {format(selectedDate, "MMMM yyyy")}
+      <Text fontSize='xl' mb={4}>
+        {format(selectedDate, 'MMMM yyyy')}
       </Text>
       {renderCalendar()}
       {selectedDate && renderHours()}
-      <ButtonGroup variant="outline" spacing="6" display={"flex"} justifyContent={"center"} marginTop={39}>
-        <Button bg="transparent" color="green.500" borderColor="green.500" _hover={{ bg: "green.500", color: "white" }}>Confirmar</Button>
-        <Button bg="transparent" color="red.500" borderColor="red.500" _hover={{ bg: "red.500", color: "white" }}>Cancelar</Button>
+      <ButtonGroup
+        variant='outline'
+        spacing='6'
+        display={'flex'}
+        justifyContent={'center'}
+        marginTop={39}
+      >
+        <Button
+          bg='transparent'
+          color='green.500'
+          borderColor='green.500'
+          _hover={{ bg: 'green.500', color: 'white' }}
+        >
+          Confirmar
+        </Button>
+        <Button
+          bg='transparent'
+          color='red.500'
+          borderColor='red.500'
+          _hover={{ bg: 'red.500', color: 'white' }}
+        >
+          Cancelar
+        </Button>
       </ButtonGroup>
     </Box>
   );
