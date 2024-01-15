7634e0f1bacc788df6fa26b48ab0324a838e005a
Gomez-Oscar <alexgomez212121@gmail.com>
2024-01-14 23:12:04 -0500

    implementando mejoras a la interfaz de perfil de usuarios para web

--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Heading,
   Image,
@@ -14,27 +14,31 @@ import {
   Text,
   Box,
   Avatar,
-} from "@chakra-ui/react";
+  HStack,
+  Divider,
+  Tooltip,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-import { FaEdit } from "react-icons/fa";
-import { FaCheck } from "react-icons/fa";
-import { FaWindowClose } from "react-icons/fa";
+import { FaEdit } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa';
+import { FaWindowClose } from 'react-icons/fa';
 import {
   deleteUserAccount,
   logoutAsync,
   updateUserName,
-} from "../../store/users/userActions";
-import Swal from "sweetalert2";
+} from '../../store/users/userActions';
+import Swal from 'sweetalert2';
 
-import { useDispatch, useSelector } from "react-redux";
-import { firestore } from "../../firebase/firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
+import { useDispatch, useSelector } from 'react-redux';
+import { firestore } from '../../firebase/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
 
-import "./userProfile.scss";
-import { sweetAlert } from "../../utils/alerts";
+import './userProfile.scss';
+import { sweetAlert } from '../../utils/alerts';
 
 const UserProfile = () => {
-  const { user } = useSelector((store) => store.user);
+  const { user } = useSelector(store => store.user);
   const dispatch = useDispatch();
 
   function EditableControls() {
@@ -46,15 +50,15 @@ const UserProfile = () => {
     } = useEditableControls();
 
     return isEditing ? (
-      <ButtonGroup justifyContent="center" size="sm">
+      <ButtonGroup justifyContent='left' size='md' w='full' spacing={2} mt={2}>
         <IconButton icon={<FaCheck />} {...getSubmitButtonProps()} />
         <IconButton icon={<FaWindowClose />} {...getCancelButtonProps()} />
       </ButtonGroup>
-    ) : (
-      <Flex justifyContent="center">
-        <IconButton size="sm" icon={<FaEdit />} {...getEditButtonProps()} />
+    ) : null; /* (
+      <Flex justifyContent='left'>
+        <IconButton size='md' icon={<FaEdit />} {...getEditButtonProps()} />
       </Flex>
-    );
+    ); */
   }
 
   const handleLogout = () => {
@@ -63,23 +67,23 @@ const UserProfile = () => {
 
   const handleDeleteAccount = () => {
     Swal.fire({
-      title: "¿Estás seguro?",
-      text: "Esto cambios no se pueden revertir",
-      icon: "warning",
+      title: '¿Estás seguro?',
+      text: 'Esto cambios no se pueden revertir',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      cancelButtonText: "Cancelar",
-      confirmButtonText: "Eliminar",
-    }).then(async (result) => {
+      confirmButtonColor: '#FF6666',
+      cancelButtonColor: '#B0E0E6',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Eliminar',
+    }).then(async result => {
       if (result.isConfirmed) {
         dispatch(deleteUserAccount(user));
-        sweetAlert("success", "Eliminada!", "Tu cuenta ha sido eliminada");
+        sweetAlert('success', 'Eliminada!', 'Tu cuenta ha sido eliminada');
       }
     });
   };
 
-  const handleUpdateName = (displayName) => {
+  const handleUpdateName = displayName => {
     dispatch(updateUserName(user, displayName));
   };
 
@@ -142,59 +146,74 @@ const UserProfile = () => {
         </Button>
       </section>
     </main> */
-    <Flex align="center" justify="center" height="100vh">
+    <Flex align='center' justify='center' height='100vh'>
       <Box
-        width={{ base: "100%", md: "50%" }}
+        // width={{ base: '100%', md: '50%' }}
+        width='500px'
         p={6}
-        borderWidth="1px"
-        borderRadius="lg"
-        boxShadow="lg"
-        mx="auto"
+        borderWidth='1px'
+        borderRadius='lg'
+        boxShadow='xl'
+        mx='auto'
       >
-        <Heading as="h1" size="xl" align="center" justify="center">
+        <Heading as='h1' size='xl' align='left' justify='center'>
           Perfil de usuario
         </Heading>
-        <Flex align="center">
+        <br />
+        <Divider />
+        <br />
+        <Flex align='center'>
           <Box mr={4}>
-            <Avatar src={user.photoURL} size="2xl" />
+            <Avatar src={user.photoURL} size='2xl' />
           </Box>
           <Box>
-            <Text>Nombre de usuario</Text>
             <Editable
-              className="main_container__profile__editable"
-              textAlign="center"
+              className='main_container__profile__editable'
+              textAlign='left'
               defaultValue={user.displayName}
-              fontSize={["20px", "25px"]}
-              isPreviewFocusable={false}
-              onSubmit={(displayName) => handleUpdateName(displayName)}
+              fontSize={['20px', '25px']}
+              isPreviewFocusable={true}
+              selectAllOnFocus={false}
+              onSubmit={displayName => handleUpdateName(displayName)}
             >
-              <EditablePreview />
-              <Input as={EditableInput} />
+              <Tooltip label='Click to edit' shouldWrapChildren={true}>
+                <EditablePreview
+                  _hover={{
+                    background: useColorModeValue('gray.100', 'gray.700'),
+                  }}
+                />
+              </Tooltip>
+              <Input py={2} px={4} as={EditableInput} />
               <EditableControls />
             </Editable>
-
-            <Text>Correo electrónico</Text>
-            <Text color="gray.500">{user.email}</Text>
+            <Text color='gray.500' fontSize='xl'>
+              {user.email}
+            </Text>
           </Box>
         </Flex>
-        <Button
-          onClick={() => handleLogout()}
-          type="submit"
-          colorScheme="blue"
-          w={{ base: "100%" }}
-          mt={4}
-        >
-          Cerrar Sesión
-        </Button>
-        <Button
-          onClick={() => handleDeleteAccount()}
-          type="submit"
-          colorScheme="red"
-          w={{ base: "100%" }}
-          mt={2}
-        >
-          Eliminar Cuenta
-        </Button>
+        <br />
+        <Divider />
+        <br />
+        <HStack>
+          <Button
+            onClick={() => handleDeleteAccount()}
+            type='submit'
+            bg='#FF6666'
+            _hover={{ bg: '#CC3333' }}
+            mt={2}
+          >
+            Eliminar Cuenta
+          </Button>
+          <Button
+            onClick={() => handleLogout()}
+            type='submit'
+            bg='#B0E0E6'
+            _hover={{ bg: '#87CEEB' }}
+            mt={2}
+          >
+            Cerrar Sesión
+          </Button>
+        </HStack>
       </Box>
     </Flex>
   );
