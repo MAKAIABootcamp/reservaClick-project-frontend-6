1ee83d36e490ac75026ee89adbdeb7b897f86f02
Gomez-Oscar <alexgomez212121@gmail.com>
2024-01-17 00:03:23 -0500

    implementado botones para cambiar de mes, visualizacion de dias de la semana, console log de dia y hora seleccionada

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
-import { Box, Grid, Button, Text, ButtonGroup } from '@chakra-ui/react';
-import { format, addDays } from 'date-fns';
-import { Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  Button,
+  Text,
+  ButtonGroup,
+  Heading,
+  Flex,
+} from '@chakra-ui/react';
+import { format, addDays, addMonths, subMonths } from 'date-fns';
+import es from 'date-fns/locale/es';
+import { useNavigate, useParams } from 'react-router-dom';
 import './calendar.scss';
-import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setStore } from '../../store/stores/storeActions';
 
 const Calendar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { storeName } = useParams();
+  const { user } = useSelector(store => store.user);
 
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedHour, setSelectedHour] = useState(null);
@@ -14,13 +27,47 @@ const Calendar = () => {
   const handleDateChange = newDate => {
     setSelectedDate(newDate);
     console.log(newDate);
-    setSelectedHour(null); // Reiniciar la hora al cambiar la fecha
+    setSelectedHour(null);
   };
 
-  const handleHourSelection = hour => {
+  const handleHourSelection = (hour, availability) => {
     setSelectedHour(hour);
-    console.log(hour);
+    if (!availability) {
+      console.log('Esta horario NO esta disponible');
+    } else {
+      selectedDate.setHours(hour.split(':').shift());
+      console.log(selectedDate);
+    }
   };
+
+  const handleConfirmReservation = () => {
+    console.log('-----------------------------');
+    console.log('Reserva exitosa!');
+    console.log(storeName.replaceAll('-', ' '));
+    console.log(user.displayName);
+    console.log(selectedDate);
+  };
+
+  const handleCancel = () => {
+    navigate('/home');
+    dispatch(setStore(null));
+  };
+
+  const schedule = {
+    '08:00': true,
+    '09:00': false,
+    '10:00': true,
+    '11:00': false,
+    '12:00': true,
+    '14:00': false,
+    '15:00': true,
+    '16:00': false,
+    '17:00': true,
+  };
+
+  const noAvailability = Object.values(schedule).every(
+    value => value === false
+  );
 
   const renderCalendar = () => {
     const daysInMonth = [];
@@ -30,50 +77,55 @@ const Calendar = () => {
       1
     );
 
+    const dayNames = [...Array(7)].map((_, index) =>
+      format(addDays(currentDate, index), 'EEEE', { locale: es })
+    );
+
     while (currentDate.getMonth() === selectedDate.getMonth()) {
       daysInMonth.push(new Date(currentDate));
       currentDate = addDays(currentDate, 1);
     }
 
+    // daysInMonth.map(day => console.log(day));
+
     return (
-      <Grid templateColumns='repeat(7, 1fr)' gap={2}>
-        {daysInMonth.map(day => (
-          <Button
-            key={day}
-            variant={
-              day.getDate() === selectedDate.getDate() ? 'solid' : 'outline'
-            }
-            colorScheme='teal'
-            onClick={() => handleDateChange(day)}
-          >
-            {format(day, 'dd')}
-          </Button>
-        ))}
-      </Grid>
+      <>
+        <Grid templateColumns='repeat(7, 1fr)' gap={2} mb={2}>
+          {dayNames.map(dayName => (
+            <Text key={dayName} textAlign='center'>
+              {dayName}
+            </Text>
+          ))}
+        </Grid>
+        <Grid templateColumns='repeat(7, 1fr)' gap={2}>
+          {daysInMonth.map(day => (
+            <Button
+              key={day}
+              variant={
+                day.getDate() === selectedDate.getDate() ? 'solid' : 'outline'
+              }
+              colorScheme={noAvailability ? 'red' : 'teal'}
+              onClick={() => handleDateChange(day)}
+            >
+              {format(day, 'dd')}
+            </Button>
+          ))}
+        </Grid>
+      </>
     );
   };
 
   const renderHours = () => {
-    const hoursOfDay = [
-      '08:00',
-      '09:00',
-      '10:00',
-      '11:00',
-      '12:00',
-      '14:00',
-      '15:00',
-      '16:00',
-      '17:00',
-    ];
+    const hours = Object.keys(schedule);
 
     return (
       <Grid templateColumns='repeat(4, 1fr)' gap={2} mt={4}>
-        {hoursOfDay.map(hour => (
+        {hours.map(hour => (
           <Button
             key={hour}
             variant={hour === selectedHour ? 'solid' : 'outline'}
-            colorScheme='teal'
-            onClick={() => handleHourSelection(hour)}
+            colorScheme={schedule[hour] ? 'teal' : 'red'}
+            onClick={() => handleHourSelection(hour, schedule[hour])}
           >
             {hour}
           </Button>
@@ -82,15 +134,39 @@ const Calendar = () => {
     );
   };
 
+  const handlePrevMonth = () => {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
+
+    if (
+      subMonths(selectedDate, 1).getFullYear() == currentYear &&
+      subMonths(selectedDate, 1).getMonth() + 1 >= currentMonth
+    ) {
+      setSelectedDate(subMonths(selectedDate, 1));
+    }
+  };
+
+  const handleNextMonth = () => {
+    setSelectedDate(addMonths(selectedDate, 1));
+  };
+
   return (
     <Box p={4} className='calendar'>
       <Heading as='h2' size='xl' display={'flex'} justifyContent={'center'}>
         {`Reserva tu cita en ${storeName.replaceAll('-', ' ')}`}
       </Heading>
-      <Text fontSize='xl' mb={4}>
-        {format(selectedDate, 'MMMM yyyy')}
-      </Text>
+      <br />
+      <Flex p={4} justifyContent={'space-between'}>
+        <Button onClick={handlePrevMonth}>Mes anterior</Button>
+        <Text fontSize='xl' mb={4}>
+          {format(selectedDate, 'MMMM yyyy', { locale: es }).toUpperCase()}
+        </Text>
+        <Button onClick={handleNextMonth}>Mes siguiente</Button>
+      </Flex>
+      <br />
       {renderCalendar()}
+      <br />
       {selectedDate && renderHours()}
       <ButtonGroup
         variant='outline'
@@ -104,6 +180,7 @@ const Calendar = () => {
           color='green.500'
           borderColor='green.500'
           _hover={{ bg: 'green.500', color: 'white' }}
+          onClick={() => handleConfirmReservation()}
         >
           Confirmar
         </Button>
@@ -112,10 +189,12 @@ const Calendar = () => {
           color='red.500'
           borderColor='red.500'
           _hover={{ bg: 'red.500', color: 'white' }}
+          onClick={() => handleCancel()}
         >
           Cancelar
         </Button>
       </ButtonGroup>
+      <br />
     </Box>
   );
 };
